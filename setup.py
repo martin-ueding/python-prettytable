f52f48f652376211f265e4c7ac8a21958b1cf8b4
Martin Ueding <dev@martin-ueding.de>
2012-01-28 17:51:24 +0100

    Add some setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
+
+from distutils.core import setup
+
+setup(
+    name = "prettytable",
+    description = "Pretty prints tables.",
+    author = "Martin Ueding",
+    author_email = "<dev@martin-ueding.de>",
+
+    py_modules = ["prettytable"]
+)
