86862da7558b80efffc67fb9d74343a6407bb722
Martin Ueding <dev@martin-ueding.de>
2012-01-29 18:58:16 +0100

    Document method

--- prettytable.py
+++ prettytable.py
@@ -24,21 +24,29 @@ def print_table(headers, data, margin=2, outfile=None):
         outfile = sys.stdout
 
     margin -= 1
+
+    # Converts the headers to strings.
     headers_str = map(str, headers)
+
+    # Converts the data to strings.
     data_str = [map(str, line) for line in data]
 
+    # Calculate the widths of the columns.
     col_widths = [max(len(headers_str[i]), max([len(row[i]) for row in data_str])) for i in range(len(headers))]
 
+    # Print the headers.
     for i in range(len(col_widths)):
         outfile.write(headers_str[i].ljust(col_widths[i])+' '*margin+" ")
 
     outfile.write("\n")
 
+    # Print a separator.
     for i in range(len(col_widths)):
         outfile.write(('-'*len(headers_str[i])).ljust(col_widths[i])+' '*margin+" ")
 
     outfile.write("\n")
 
+    # Print data.
     for row, row_str in zip(data, data_str):
         for i in range(len(col_widths)):
             outfile.write(_justify(row[i], row_str[i], col_widths[i])+' '*margin+" ")
